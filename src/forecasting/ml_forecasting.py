01419a34494a76586da60ab43b04b7da27617e80
Kedar Waghmode <kedar@stovellai.com>
2024-04-12 18:14:52 -0700

    New fixes for MacOS(Works upto Chapter 12)

--- src/forecasting/ml_forecasting.py
+++ src/forecasting/ml_forecasting.py
@@ -149,8 +149,11 @@ class FeatureConfig:
             feature_list = list(set(feature_list) - set(self.exogenous_features))
         feature_list = list(set(feature_list))
         delete_index_cols = list(set(self.index_cols) - set(self.feature_list))
+        
+        all_columns = list(set(feature_list + self.index_cols))
+
         (X, y, y_orig) = (
-            df.loc[:, list(feature_list + self.index_cols)]
+            df.loc[:, all_columns]
             .set_index(self.index_cols, drop=False)
             .drop(columns=delete_index_cols),
             df.loc[:, [self.target] + self.index_cols].set_index(
